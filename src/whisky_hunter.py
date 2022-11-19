25be580ede39aa56873d195e4c6d67af43749b55
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:33:09 +0800

    Update whisky_hunter.py

--- src/whisky_hunter.py
+++ src/whisky_hunter.py
@@ -1,34 +1,33 @@
 from requests import get
 
-
 class WhiskyHunter:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://whiskyhunter.net/api"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
 	
-	def read_auction_data(self, slug: str):
+	def read_auction_data(self, slug: str) -> dict:
 		return get(
 			f"{self.api}/auction_data/{slug}/auction_data_read",
 			headers=self.headers).json()
 	
-	def get_auctions_data(self):
+	def get_auctions_data(self) -> dict:
 		return get(
 			f"{self.api}/auctions_data/auctions_data_list",
 			headers=self.headers).json()
 	
-	def get_auctions_info(self):
+	def get_auctions_info(self) -> dict:
 		return get(
 			f"{self.api}/auctions_info/auctions_info_list",
 			headers=self.headers).json()
 	
-	def get_distilleries_info(self):
+	def get_distilleries_info(self) -> dict:
 		return get(
 			f"{self.api}/distilleries_info/distilleries_info_list",
 			headers=self.headers).json()
 	
-	def read_distillery_data(self, slug: str):
+	def read_distillery_data(self, slug: str) -> dict:
 		return get(
 			f"{self.api}/distillery_data/{slug}/distillery_data_read",
 			headers=self.headers).json()
